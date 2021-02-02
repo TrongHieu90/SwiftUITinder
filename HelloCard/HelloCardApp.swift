f9af9d2cb330a9a822ba47b843f5eda410cd1614
hieu.nguyen <hieu.nguyen@amanotes.com>
2021-02-02 16:33:21 +0700

    Initial Commit

---
+++ HelloCard/HelloCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCardApp.swift
+//  HelloCard
+//
+//  Created by Hieu.Nguyen on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
